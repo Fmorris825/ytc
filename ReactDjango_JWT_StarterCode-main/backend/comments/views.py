5751af503f251125194f2fcc0937b5ee27ed01ed
Fred Morris <FredrickCPMorris@hotmail.com>
2022-11-14 11:46:41 -0600

    Migrated new models Comment and Reply to database.

--- ReactDjango_JWT_StarterCode-main/backend/comments/views.py
+++ ReactDjango_JWT_StarterCode-main/backend/comments/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import CommentSerializer
+from .models import Comment
 
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def get_all_comments(request):
+    
+    if request.method == 'GET':
+        comments = Comment.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = CommentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.errors, status=status.HTTP_201_CREATED)
